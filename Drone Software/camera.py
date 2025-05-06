ae568f4f8d13b74ee641199179e6d9413003e51d
eirikko <eirikkongsparten@gmail.com>
2025-05-06 12:47:30 +0200

    Finished test code, tried testing with webcam on laptop. Failed to open camera.

--- Drone Software/camera.py
+++ Drone Software/camera.py
@@ -11,8 +11,9 @@ class Camera:
 
     def initialize(self):
         """Initialize the GStreamer pipeline for the camera"""
-        pipeline = f"v4l2src device={self.device} ! video/x-raw, width={self.width}, height={self.height} ! videoconvert ! appsink"
-        self.cap = cv2.VideoCapture(pipeline, cv2.CAP_GSTREAMER) #Start capture of video
+        #pipeline = f"v4l2src device={self.device} ! video/x-raw, width={self.width}, height={self.height} ! videoconvert ! appsink"
+        #self.cap = cv2.VideoCapture(pipeline, cv2.CAP_GSTREAMER) #Start capture of video
+        self.cap = cv2.VideoCapture(0)
         if not self.cap.isOpened():
             raise RuntimeError("Failed to open camera")
             #Throw error if the camera cannot be opened
