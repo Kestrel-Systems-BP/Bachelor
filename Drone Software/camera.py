0494ae4344a6bf96625009b39e336fdfaab2412f
eirikko <eirikkongsparten@gmail.com>
2025-05-06 11:37:03 +0200

    Added function to capture the frames from camera

--- Drone Software/camera.py
+++ Drone Software/camera.py
@@ -10,7 +10,17 @@ class Camera:
     def initialize(self):
         """Initialize the GStreamer pipeline for the camera"""
         pipeline = f"v4l2src device={self.device} ! video/x-raw, width={self.width}, height={self.height} ! videoconvert ! appsink"
-        self.cap = cv2.VideoCapture(pipeline, cv2.CAP_GSTREAMER)
+        self.cap = cv2.VideoCapture(pipeline, cv2.CAP_GSTREAMER) #Start capture of video
         if not self.cap.isOpened():
-            raise RuntimeError("Failed to initialize camera")
+            raise RuntimeError("Failed to open camera")
+            #Throw error if the camera cannot be opened
         return True
+
+    def get_frame(self):
+        """Capture a single frame"""
+        if not self.cap:
+            raise RuntimeError("Camera not initialized")
+        ret, frame = self.cap.read()
+        if not ret:
+            raise RuntimeError("Failed to grab frame")
+        return frame
