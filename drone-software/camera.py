d6f53e0adf295e76961771fda9cce8497c8f9c8f
eirikko <eirikkongsparten@gmail.com>
2025-05-14 11:59:09 +0200

    Camera is now accessed through RTSP stream. Commands like imshow commented out. These commands do not work without GUI.

--- drone-software/camera.py
+++ drone-software/camera.py
@@ -11,14 +11,21 @@ class Camera:
         self.height = height
         self.cap = None
 
+#    def initialize(self):
+#        """Initialize the GStreamer pipeline for the camera"""
+#        pipeline = f"v4l2src device={self.device} ! video/x-raw, width={self.width}, height={self.height} ! videoconvert ! appsink"
+#        self.cap = cv2.VideoCapture(pipeline, cv2.CAP_GSTREAMER) #Start capture of video
+#        #self.cap = cv2.VideoCapture(0)
+#        if not self.cap.isOpened():
+#            raise RuntimeError("Failed to open camera")
+#            #Throw error if the camera cannot be opened
+#        return True
+
     def initialize(self):
-        """Initialize the GStreamer pipeline for the camera"""
-        pipeline = f"v4l2src device={self.device} ! video/x-raw, width={self.width}, height={self.height} ! videoconvert ! appsink"
-        self.cap = cv2.VideoCapture(pipeline, cv2.CAP_GSTREAMER) #Start capture of video
-        #self.cap = cv2.VideoCapture(0)
+        rtsp_url = "rtsp://127.0.0.1:8904/live"
+        self.cap = cv2.VideoCapture(rtsp_url, cv2.CAP_FFMPEG)
         if not self.cap.isOpened():
-            raise RuntimeError("Failed to open camera")
-            #Throw error if the camera cannot be opened
+            raise RuntimeError("Failed to open RTSP stream")
         return True
 
     def get_frame(self):
@@ -41,10 +48,10 @@ if __name__ == "__main__":
         cam = Camera()
         cam.initialize()
         frame = cam.get_frame()
-        cv2.imshow("Test Camera", frame)
-        cv2.waitKey(0)
-        cam.release()
-        cv2.destroyAllWindows()
+#        cv2.imshow("Test Camera", frame)
+#        cv2.waitKey(0)
+#        cam.release()
+#        cv2.destroyAllWindows()
         print("Camera test success")
     except Exception as e:
         print(f"Camera test failed: {e}")
